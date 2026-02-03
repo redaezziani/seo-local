2bf2414e65050f9583a886b04afe0d1df9d02bc0
redaezziani <klausdev2@gmail.com>
2026-02-03 18:11:35 +0100

    feat: Configure Vercel adapter and update package dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,12 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 import sanity from '@sanity/astro';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   vite: {
       plugins: [tailwindcss()],
 	},
